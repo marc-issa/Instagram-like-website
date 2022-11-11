7840129bcca63a6fc9591bc8be0456c9d74e9a49
Marc Issa <issamarc15@gmail.com>
2022-11-11 14:27:58 +0200

    Creating a function to check if the email already exists

--- api/check_avail.php
+++ api/check_avail.php
@@ -1,12 +1,30 @@
 <?php
 
-    function check_username($username, $conn){
+    function checkUsername($username, $conn){
         $stmt = $conn->prepare("SELECT id FROM users WHERE username=?");
         
         if($conn->connect_error){
             die("Connection Failed: ".$conn->connect_error);
         }else{
             $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
+            
+            if($user){
+                return true;
+            }
+            return false;
+        }
+    }
+
+    function checkEmail($email, $conn){
+        $stmt = $conn->prepare("SELECT id FROM users WHERE email=?");
+        
+        if($conn->connect_error){
+            die("Connection Failed: ".$conn->connect_error);
+        }else{
+            $stmt->bind_param("s", $email);
             $stmt->execute();
             $result = $stmt->get_result();
             $user = $result->fetch_assoc();
