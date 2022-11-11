b45ff637e5c93345278ad34044011b5f789780d3
Marc Issa <issamarc15@gmail.com>
2022-11-11 17:30:57 +0200

    Finishing the add and remove like api

--- api/views/add_remove_like.php
+++ api/views/add_remove_like.php
@@ -1,8 +1,29 @@
 <?php
     include("../connectiondb.php");
 
-    $post_id = $_GET["post_id"];
-    $user_id = $_GET["user_id"];
+    $post_id = $_POST["post_id"];
+    $user_id = $_POST["user_id"];
+
+    $response = [];
+
+    $stmt = $conn->prepare("SELECT * FROM likes WHERE post_id = ? and user_id = ?");
+    if($conn->connect_error){
+        die("Connection Failed: ".$conn->connect_error);
+    }else{
+        $stmt->bind_param("ii", $post_id, $user_id);
+        $stmt->execute();
+        $results = $stmt->get_result();
+        $like = $results->fetch_assoc();
+
+        if(!$like){
+            addLike($conn, $post_id, $user_id);
+            $response["resp"] = "Like added";
+        }else{
+            removeLike($conn, $post_id, $user_id);
+            $response["resp"] = "Like removed";
+        }
+        echo json_encode($response);
+    }
 
     function addLike($conn, $post_id, $user_id){
         $stmt = $conn->prepare("INSERT INTO likes(post_id, user_id) VALUES(?,?)");
