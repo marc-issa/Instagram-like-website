151c7cdf201e6aaf80f8ad269cd4ffe203817e44
Marc Issa <issamarc15@gmail.com>
2022-11-11 14:36:16 +0200

    Creating a function to get the id based on the username given

--- api/check_avail.php
+++ api/check_avail.php
@@ -35,4 +35,19 @@
             return false;
         }
     }
+
+    function getId($username, $conn){
+        $stmt = $conn->prepare("SELECT id FROM users WHERE $username=?");
+        
+        if($conn->connect_error){
+            die("Connection Failed: ".$conn->connect_error);
+        }else{
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
+            
+            return $user;
+        }
+    }
 ?>
