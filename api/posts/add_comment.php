fcc336217a28bc2dec17a09080891843a54d8dfa
Marc Issa <issamarc15@gmail.com>
2022-11-12 14:40:03 +0200

    Changing the code of add comment to be shorter

--- api/posts/add_comment.php
+++ api/posts/add_comment.php
@@ -7,29 +7,12 @@
 
     $response = [];
 
-    $stmt = $conn->prepare("SELECT * FROM comments WHERE post_id = ? and user_id = ?");
+    $stmt = $conn->prepare("INSERT INTO comments(com, post_id, user_id) VALUES(?,?,?)");
+        
     if($conn->connect_error){
         die("Connection Failed: ".$conn->connect_error);
     }else{
-        $stmt->bind_param("ii", $post_id, $user_id);
+        $stmt->bind_param("sii", $com, $post_id, $user_id);
         $stmt->execute();
-        $results = $stmt->get_result();
-        $like = $results->fetch_assoc();
-
-        addComment($conn, $post_id, $user_id, $com);
-        $response["resp"] = "Comment added";
-        $response["Comment-content"] = $com;
-        
-        echo json_encode($response);
-    }
-
-    function addComment($conn, $post_id, $user_id, $com){
-        $stmt = $conn->prepare("INSERT INTO comments(com, post_id, user_id) VALUES(?,?,?)");
-        if($conn->connect_error){
-            die("Connection Failed: ".$conn->connect_error);
-        }else{
-            $stmt->bind_param("sii", $com, $post_id, $user_id);
-            $stmt->execute();
-        }
     }
 ?>
