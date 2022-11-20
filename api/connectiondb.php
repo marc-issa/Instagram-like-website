a9a49a172424ecf0ed7674eac661c53c2f433a9f
Marc Issa <issamarc15@gmail.com>
2022-11-20 19:28:51 +0200

    Removing the echo in the connection file

--- api/connectiondb.php
+++ api/connectiondb.php
@@ -5,11 +5,4 @@
     $db = "instagram-like-app";
 
     $conn = new mysqli($host, $username, $password, $db);
-    
-    if($conn){
-        echo "Connected";
-    }
-    else{
-        echo "Error Connecting to database";
-    }
 ?>
