7ed500a44a7a9e49223d75a3f8b7eced0f128d8f
Marc Issa <issamarc15@gmail.com>
2022-12-14 00:12:38 +0200

    Changing the modal for posts

--- website/modal-scripts.js
+++ website/modal-scripts.js
@@ -1,14 +1,19 @@
-const overlay = document.getElementById("overlay");
-
 function viewPost() {
-    console.log("opening")
     const modal = document.getElementById("modal");
+    const post_modal = document.getElementById("modal-post");
     modal.classList.add('active')
-    overlay.classList.add('active')
+    modal.classList.add('post')
+    post_modal.classList.add('active')
 }
 
 function closePost() {
     const modal = document.getElementById("modal");
+    const post_modal = document.getElementById("modal-post");
     modal.classList.remove('active')
-    overlay.classList.remove('active')
+    modal.classList.remove('post')
+    post_modal.classList.remove('active')
+}
+
+function viewStorie() {
+
 }
