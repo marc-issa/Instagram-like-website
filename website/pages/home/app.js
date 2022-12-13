f4d9f1bd555c4ca71efd3b7434efb609e0ec33aa
Marc Issa <issamarc15@gmail.com>
2022-12-13 14:32:23 +0200

    Changing the name of the files in the home directorie

---
+++ website/pages/home/app.js
@@ -0,0 +1,11 @@
+let post_bt = document.getElementById("comment-post-bt");
+post_bt.href = 'javascript:void(0)';
+
+function isTyping() {
+    post_bt.href = '#';
+    post_bt.style.color = "#0095f6";
+}
+
+function profileRedirect() {
+    console.log("Redirecting to home page");
+}
