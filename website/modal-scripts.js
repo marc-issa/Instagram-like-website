5ef3b25800818857fdf90a3581e523c603989618
Marc Issa <issamarc15@gmail.com>
2022-12-14 00:36:14 +0200

    Adding transition to the popup and Adding a function to disable scroll when the pop up is opened

--- website/modal-scripts.js
+++ website/modal-scripts.js
@@ -1,19 +1,43 @@
 function viewPost() {
     const modal = document.getElementById("modal");
     const post_modal = document.getElementById("modal-post");
-    modal.classList.add('active')
-    modal.classList.add('post')
-    post_modal.classList.add('active')
-}
 
-function closePost() {
-    const modal = document.getElementById("modal");
-    const post_modal = document.getElementById("modal-post");
-    modal.classList.remove('active')
-    modal.classList.remove('post')
-    post_modal.classList.remove('active')
+    modal.classList.add('active');
+    modal.classList.add('post');
+    post_modal.classList.add('active');
+
+    disableScrolling();
 }
 
 function viewStorie() {
+    const modal = document.getElementById("modal");
+    const storie_modal = document.getElementById("modal-storie");
 
+    modal.classList.add('active');
+    modal.classList.add('storie');
+
+    disableScrolling();
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    const post_modal = document.getElementById("modal-post");
+    const storie_modal = document.getElementById("modal-storie");
+
+    modal.classList.remove('active');
+    modal.classList.remove('post');
+    modal.classList.remove('storie');
+    post_modal.classList.remove('active');
+
+    enableScrolling();
+}
+
+function disableScrolling() {
+    let x = window.scrollX;
+    let y = window.scrollY;
+    window.onscroll = function () { window.scrollTo(x, y); };
+}
+
+function enableScrolling() {
+    window.onscroll = function () { };
 }
