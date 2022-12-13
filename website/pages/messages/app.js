7ab7ce27b35e4b0623c2319add12381c6571f6a3
Marc Issa <issamarc15@gmail.com>
2022-12-14 01:34:40 +0200

    Creating the display of the chat when the user click on a room

--- website/pages/messages/app.js
+++ website/pages/messages/app.js
@@ -1,7 +1,15 @@
-let send_bt = document.getElementById("chat-post-bt");
-send_bt.href = 'javascript:void(0)';
+let last_index = -1;
+function activateRoom(index) {
+    let active_room = document.querySelectorAll('.room-item')[index];
+    let last_room = document.querySelectorAll('.room-item')[last_index];
+    let chat = document.getElementById('chat');
 
-function isTyping() {
-    send_bt.href = '#';
-    send_bt.style.color = "#0095f6";
+    active_room.classList.add("active-room")
+    chat.classList.add("active-chat")
+
+    if (last_room != undefined) {
+        last_room.classList.remove("active-room");
+    }
+
+    last_index = index;
 }
