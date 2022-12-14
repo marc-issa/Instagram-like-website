6e3dabd5f23a51f6a09be38953232a91f7e5aff7
Marc Issa <issamarc15@gmail.com>
2022-12-14 22:20:41 +0200

    Finishing the edit page design

--- website/pages/edit/app.js
+++ website/pages/edit/app.js
@@ -1,3 +1,36 @@
+
+
+/* ****************************** */
+/*          Change Display        */
+/* ****************************** */
+
+/* Default option and display */
+const edit_profile = document.getElementById("edit-profile");
+const profile_display = document.getElementById("profile-display");
+
+const change_password = document.getElementById("change-password")
+const change_display = document.getElementById("change-display")
+
+edit_profile.classList.add('active-option')
+profile_display.classList.add('active-display')
+
+function changeContent(num) {
+    console.log(num)
+    if (num == 0) {
+        edit_profile.classList.add('active-option')
+        profile_display.classList.add('active-display')
+
+        change_password.classList.remove('active-option')
+        change_display.classList.remove('active-display')
+    } else {
+        change_password.classList.add('active-option')
+        change_display.classList.add('active-display')
+
+        edit_profile.classList.remove('active-option')
+        profile_display.classList.remove('active-display')
+    }
+}
+
 /* ****************************** */
 /*           Delete Modal         */
 /* ****************************** */
