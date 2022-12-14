599f366af57cd948b6a76bf66118d32de9d7ec33
Marc Issa <issamarc15@gmail.com>
2022-12-14 02:45:59 +0200

    Creating the notification popup module

--- website/modal-scripts.js
+++ website/modal-scripts.js
@@ -20,16 +20,49 @@ function viewStorie() {
     disableScrolling();
 }
 
+function viewNotif() {
+    const modal = document.getElementById("modal");
+    const notif_modal = document.getElementById("modal-notif");
+    const bt_link = document.getElementById("close-bt-link")
+
+    modal.classList.add('active');
+    modal.classList.add('notif');
+
+    notif_modal.classList.add('active')
+
+    bt_link.classList.add("deactivate-bt")
+
+    disableScrolling();
+}
+
+function closeNotif() {
+    const modal = document.getElementById("modal");
+    const notif_modal = document.getElementById("modal-notif");
+    const bt_link = document.getElementById("close-bt-link")
+
+    modal.classList.remove('active');
+    notif_modal.classList.remove('active');
+
+    modal.classList.remove('notif');
+    bt_link.classList.remove("deactivate-bt")
+
+}
+
 function closeModal() {
     const modal = document.getElementById("modal");
     const post_modal = document.getElementById("modal-post");
     const storie_modal = document.getElementById("modal-storie");
 
+
     modal.classList.remove('active');
-    modal.classList.remove('post');
-    modal.classList.remove('storie');
     storie_modal.classList.remove('active')
     post_modal.classList.remove('active');
+
+
+    modal.classList.remove('post');
+    modal.classList.remove('storie');
+    modal.classList.remove('notif');
+
 
     enableScrolling();
 }
