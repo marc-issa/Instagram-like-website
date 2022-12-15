73cb901f676307b1c93cc555085f9f598747ee1a
Marc Issa <issamarc15@gmail.com>
2022-12-15 16:53:27 +0200

    Creating follows table, migration, controller, model

---
+++ api/database/migrations/2022_12_15_144934_create_follows_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_following");
+            $table->unsignedBigInteger("user_followed");
+            $table->foreign("user_followed")->references("id")->on("users");
+            $table->foreign("user_following")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+};
