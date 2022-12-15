8ddc04013c84cf03277185c5d614bb51b651f7d1
Marc Issa <issamarc15@gmail.com>
2022-12-15 16:47:19 +0200

    Creating block table, migration, model, controller

---
+++ api/database/migrations/2022_12_15_144412_create_blocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_blocking");
+            $table->unsignedBigInteger("user_blocked");
+            $table->foreign("user_blocking")->references("id")->on("users");
+            $table->foreign("user_blocked")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+};
