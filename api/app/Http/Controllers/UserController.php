d06547df303e7e3549e8260471a84c7bc58c1f80
Marc Issa <issamarc15@gmail.com>
2022-12-16 01:44:58 +0200

    Creating edit porfile api

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -13,6 +13,7 @@ class UserController extends Controller
 
     function editProfile(Request $request){
         $user_token = Auth::user();
+
         if($request->username){
             $username_val = User::where("username", $request->username)->first();
             if($username_val!=null){
@@ -25,7 +26,45 @@ class UserController extends Controller
                 $user->update(["username"=>$request->username]);
             }
         }
-        $email_val = User::where("email", $request->email)->first();
+
+        if($request->email){
+            $email_val = User::where("email", $request->email)->first();
+            if($email_val!=null){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'email-exists',
+                ], 401);
+            }else{
+                $user = User::where("email",$user_token["email"]);
+                $user->update(["email"=>$request->email]);
+            }
+        }
+
+        if($request->name){
+            $user = User::where("id",$user_token["id"]);
+            $user->update(["name"=>$request->name]);
+        }
+
+        if($request->bio){
+            $user = User::where("id",$user_token["id"]);
+            $user->update(["bio"=>$request->bio]);
+        }
+
+        if($request->gender){
+            $user = User::where("id",$user_token["id"]);
+            $user->update(["gender"=>$request->gender]);
+        }
+
+        if($request->gender){
+            $user = User::where("id",$user_token["id"]);
+            $user->update(["gender"=>$request->gender]);
+        }
+
+        if($request->profile_img){
+            $user = User::where("id",$user_token["id"]);
+            $user->update(["profile_img"=>$request->profile_img]);
+        }
+        
     }
 
     function getUser(){
