9daf3529fa6ef81e73c7bb28e20cdb85a217e764
Marc Issa <issamarc15@gmail.com>
2022-12-16 03:10:22 +0200

    Adding delete button to the post modal

--- website/pages/home/app.js
+++ website/pages/home/app.js
@@ -1,6 +1,6 @@
-if (localStorage.getItem("token") == null) {
-    window.location.href = "http://localhost/Projects/Websites/Instagram-like-website/website/pages/login/login.html";
-}
+// if (localStorage.getItem("token") == null) {
+//     window.location.href = "http://localhost/Projects/Websites/Instagram-like-website/website/pages/login/login.html";
+// }
 
 let post_bt = document.getElementById("comment-post-bt");
 post_bt.href = 'javascript:void(0)';
